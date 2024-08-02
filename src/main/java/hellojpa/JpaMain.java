4324bd2ec00bcf291b3ba32e7090a9647870b9c6
suhyeon1032 <daram_9@naver.com>
2024-08-02 16:36:56 +0900

    자바 ORM 표준 JPA 프로그래밍 기본편 - 엔티티 매핑 실습 코드

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,24 +16,22 @@ public class JpaMain {
         tx.begin();
 
         try {
+            /*
 
             // 영속
-            Member member1 = new Member(150L, "A");
-            Member member2 = new Member(160L, "B");
-
-            em.persist(member1);
-            em.persist(member2);
-
-            em.flush(); // 쿼리 강제 실행 (commit 전 쓰기 지연 SQL 에 저장된 쿼리들이 즉시 반영)
+//            Member member1 = new Member(150L, "A");
+//            Member member2 = new Member(160L, "B");
+//
+//            em.persist(member1);
+//            em.persist(member2);
+//
+//            em.flush(); // 쿼리 강제 실행 (commit 전 쓰기 지연 SQL 에 저장된 쿼리들이 즉시 반영)
             // flush 는 영속성 컨텍스트를 비우지 않음!!
 
-            /*
             // db 수정 코드 // 변경감지
             Member findMember = em.find(Member.class, 1L);
             findMember.setName("HelloJPA"); // em.persist(member); 수정시엔 코드 입력하지 않아도 된다.
-            */
 
-            /*
             // 페이징
             List<Member> result = em.createQuery("select m from Member as m where m.id = 1", Member.class)
                     .setFirstResult(0) // setFirstResult 부터 setMaxResults 까지 페이징 // 0번부터 10번까지
@@ -42,9 +40,8 @@ public class JpaMain {
 
             for (Member member : result) {
                 System.out.println("member.name = " + member.getName());
-            }*/
+            }
 
-            /*
             // 객체를 생성하기만 한 비영속 상태
             Member member = new Member(); // insert
             member.setId(3L);
@@ -53,12 +50,18 @@ public class JpaMain {
             // 영속 상태 // 영속상태가 된다해서 쿼리가 실행되지는 않는다.
             em.persist(member); // 1차 캐시에 저장 & insert SQL 생성 후 쓰기 지연 SQL 저장소에 저장 (아직 커밋 안됨 tx.commit(); 시 실행)
             // **실무에선 em.persist(member); 만 호출해도 Spring 이 코드를 해결해준다.**
-            */
 
-            /*
             // 엔티티 삭제
             em.remove(findMember);
-            * */
+            *
+            */
+
+            Member member = new Member();
+//            member.setId("ID_A");
+            member.setUsername("C");
+
+            em.persist(member);
+
             tx.commit(); // 쓰기 지연 SQL 저장소에 있던 쿼리문들이 flush 됨
         } catch (Exception e) {
             tx.rollback();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -2,27 +2,27 @@ package hellojpa;
 
 import jdk.jfr.Name;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
+@TableGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        table = "MY_SEQUENCES",
+        pkColumnValue = "MEMBER_SEQ", allocationSize = 1)
 public class Member {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.TABLE,
+            generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
-    @Name("name") // DB Member Table에서 해당 컬럼(name) 지정
-    private String name;
+
+//    @Name("name") // DB Member Table에서 해당 컬럼(name) 지정
+    @Column(name = "name", nullable = false) // DB 컬럼명은 name 으로 지정
+    private String username;
 
     public Member(){ // 기본 생성자
     }
-
-
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    //Getter, Setter ...
 
     public Long getId() {
         return id;
@@ -32,11 +32,11 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
